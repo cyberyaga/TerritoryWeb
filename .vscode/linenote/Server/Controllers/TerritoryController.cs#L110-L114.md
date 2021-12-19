9f298a5dc2e8857a4eb7b3b4be771204acca150f
cyberyaga <cyberyaga@hotmail.com>
2021-12-19 11:09:43 -0500

    Fixed Territory Create Radio.

---
+++ .vscode/linenote/Server/Controllers/TerritoryController.cs#L110-L114.md
@@ -0,0 +1,3 @@
+Need to get specific user doing the action.
+Need to get current CongregationID
+Need to fix Territory Type Value
---
+++ Shared/Territory/TerritoryCreateView.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace TerritoryWeb.Shared.Territory
+{
+    public class TerritoryCreateView
+    {
+        public List<TerritoryType>? TerritoryTypes { get; set; }
+    }
+
+    public class TerritoryType
+    {
+        public int TerritoryTypeId { get; set; }
+        public string? TerritoryTypeDescription { get; set; }
+    }
+}
